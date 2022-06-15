f8bac241336e62a4fc76ed69acd3d956e80f09ce
Nicholas Mutale <nicomutale@gmail.com>
2022-06-15 18:54:42 +0200

    Fix linter errors

--- test/controllers/post_controller_test.rb
+++ test/controllers/post_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class PostControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get post_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get post_show_url
     assert_response :success
   end
