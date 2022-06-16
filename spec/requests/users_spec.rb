f522e60d4808b1cffa42ff79d11419cfd1c88ead
Nicholas Mutale <nicomutale@gmail.com>
2022-06-16 08:51:33 +0200

    Add rails-controller-testing gem update user spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET /users' do
-        it 'works! (now write some real specs)' do
-        get users_path
-        expect(response).to have_http_status(200)
-        expect(response.body).to include('Users')
+    describe 'GET /index' do
+        context 'when the page is opened' do
+            it 'returns a correct response' do
+                get '/users'
+                expect(response).to have_http_status(200)
+                expect(response.body).to include('<h1>Users</h1>')
+                expect(response.body).to include('<p>Show all users</p>')
+                # expect(response.body).to render_template('users/index')
         end
     end
     end
+end
