15ec026b43457da9cc1b3e1f9534cf6a235fafd3
Nicholas Mutale <nicomutale@gmail.com>
2022-06-16 11:18:51 +0200

    fix linter error

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     context 'when the page is opened' do
       it 'returns a correct response' do
-        get '/posts'
+        get '/users/1/posts'
         expect(response).to have_http_status(200)
         expect(response.body).to include('<h1>Posts</h1>')
-        expect(response.body).to include('<p>Show all posts</p>')
+        expect(response.body).to include('<p>Here is a list of posts for a given user</p>')
         expect(response.body).to render_template('posts/index')
       end
     end
@@ -15,10 +15,10 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /show' do
     context 'when the page is opened' do
       it 'returns a correct response' do
-        get '/posts/1'
+        get '/users/1/posts/1'
         expect(response).to have_http_status(200)
         expect(response.body).to include('<h1>Posts</h1>')
-        expect(response.body).to include('<p>Show post details</p>')
+        expect(response.body).to include('<p>Show one post for a given user</p>')
         expect(response.body).to render_template('posts/show')
       end
     end
