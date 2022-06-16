aafe640d15f05112c6ff91c24cc5874c0518ecf6
Nicholas Mutale <nicomutale@gmail.com>
2022-06-16 08:59:38 +0200

    Add tests for users on show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,8 +8,22 @@ RSpec.describe 'Users', type: :request do
                 expect(response).to have_http_status(200)
                 expect(response.body).to include('<h1>Users</h1>')
                 expect(response.body).to include('<p>Show all users</p>')
-                # expect(response.body).to render_template('users/index')
+                expect(response.body).to render_template('users/index')
         end
     end
     end
+
+    describe 'GET /show' do
+        context 'when the page is opened' do
+            it 'returns a correct response' do
+                get '/users/1'
+                expect(response).to have_http_status(200)
+                expect(response.body).to include('<h1>Users</h1>')
+                expect(response.body).to include('<p>Show user details</p>')
+                expect(response.body).to render_template('users/show')
+            end
+        end
+    end
+
+
 end
