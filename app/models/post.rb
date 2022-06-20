c6588a986469108b1a689fd59e99a52eaa8e784a
Nicholas Mutale <nicomutale@gmail.com>
2022-06-20 20:32:47 +0200

    Add post method Add user method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
