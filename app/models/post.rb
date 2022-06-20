6dbdaaa5f886bc1bf1381a48222720f2ee9de8aa
Nicholas Mutale <nicomutale@gmail.com>
2022-06-20 19:00:00 +0200

    Set up post associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :users
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 end
