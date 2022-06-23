cb73c70a23d690961a18fee04c69d9716b406d17
Nicholas Mutale <nicomutale@gmail.com>
2022-06-23 12:37:53 +0200

    Add form define create in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,10 @@
 class LikesController < ApplicationController
-    def new
+    def create
+        like = Like.new(author: current_user, post: current_post)
+        if like.save
+            redirect_to user_post_url(current_user, current_post)
+        else
+            redirect_to{new_user_post(current-user)}
+        end
     end
 end
