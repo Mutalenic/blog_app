889a4eb0ae1645d69cc873995b8728b0b6ed3c25
Nicholas Mutale <nicomutale@gmail.com>
2022-06-23 12:08:26 +0200

    Update routes Add views to spec create new comments html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'likes/new'
+  get 'comments/new'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show] do
       resources :comments, only: [:create, :new]
       resources :likes, only: [:create, :new]
     end
   end
-  root "users#index"
+  root 'users#index'
 end
