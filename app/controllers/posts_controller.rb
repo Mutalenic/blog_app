eb13dcbf07fb68026a19d6f47eb9237b382b4530
Nicholas Mutale <nicomutale@gmail.com>
2022-06-23 13:32:11 +0200

    Create actions in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @new_post = Post.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(post_params)
+    @post.author = @user
+    if @post.save
+      redirect_to user_post_url(id: @post.id, user_id: @user.id)
+    else
+      render :new
+    end
+  end
+
+    def post-params
+      params.require(:post).permit(:title, :body)
+    end
 end
