05798d2a7d0921174bc664e4be9acae5266956cd
Nicholas Mutale <nicomutale@gmail.com>
2022-06-24 11:16:14 +0200

    Add flash messages updated

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,8 @@
 class LikesController < ApplicationController
-  def create
+    def create
     like = Like.new(author: current_user, post: current_post)
     if like.save
+      flash[:success] = 'Post liked!'
       redirect_to user_post_url(current_user, current_post)
     else
       redirect_to { new_user_post(current - user) }
