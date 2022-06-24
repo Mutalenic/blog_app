1a218e176d4c7a5c1fd363ff67bce1249b9c2f31
Nicholas Mutale <nicomutale@gmail.com>
2022-06-24 14:12:48 +0200

    Add test for like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations for the like model' do
+    before(:each) do
+      @like = Like.new(author_id: 1, post_id: 1)
+    end
+
+    before{@like}
+
+    it 'if author_id is not present, it should not be valid' do
+      @like.author_id = nil
+      expect(@like).to_not be_valid
+    end
+
+    it 'if post_id is not present, it should not be valid' do
+      @like.post_id = nil
+      expect(@like).to_not be_valid
+    end
+  end
 end
