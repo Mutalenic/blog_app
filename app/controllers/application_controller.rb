bf66dbd77a462294b224c8f8a7aee6a598c8819f
Nicholas Mutale <nicomutale@gmail.com>
2022-06-27 16:58:34 +0200

    Configure user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  before_action :update_allowed_params, if: :devise_controller?
+
+  protected
+
+  def updated_allowed_params
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+
+  
     def all_users
     User.all
   end
