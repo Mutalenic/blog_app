3dfe7ef403db70c5d45fcaa3192128b181039b75
Nicholas Mutale <nicomutale@gmail.com>
2022-06-27 18:26:47 +0200

    Fix nomethoderror in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-  before_action :update_allowed_params, if: :devise_controller?
+  before_action :updated_allowed_params, if: :devise_controller?
 
   protected
 
