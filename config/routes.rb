d4a9526a114e936dc816285e171e8f8d3c01f05d
Nicholas Mutale <nicomutale@gmail.com>
2022-06-29 15:20:36 +0200

    Add destroy to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'likes/new'
   get 'comments/new'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :create, :new, :show] do
-      resources :comments, only: [:create, :new]
+    resources :posts, only: [:index, :create, :new, :show, :destory] do
+      resources :comments, only: [:create, :new, :destory]
       resources :likes, only: [:create, :new]
     end
   end
