96371944a35da9dfb381e49c90425a4c02ffdee8
Nicholas Mutale <nicomutale@gmail.com>
2022-06-30 22:48:56 +0200

    Add more tests to user show page

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -7,6 +7,11 @@ RSpec.feature 'Testing user show page', type: :feature do
                          bio: 'Developer', email: 'nico@gmail.com', password: 'password',
                          confirmed_at: Time.now, role: 'admin', posts_counter: 0)
 
+    7.times do |i|
+     @p = Post.create(author: @first, title: "Post #{i}", text: "This is a post #{i}",
+                  likes_counter: 0, comments_counter: 0)
+    end
+
     visit user_session_path
 
     within 'form' do
@@ -35,4 +40,21 @@ RSpec.feature 'Testing user show page', type: :feature do
     click_link('See all posts')
     expect(current_path).to eq user_posts_path(User.first.id)
   end
+
+  scenario "I can see the number of posts a user has written" do
+    expect(page).to have_content('Number of posts: 7')
+  end
+
+  scenario "I can see the user's first 3 posts" do
+    expect(page).to have_content('This is a post 6')
+    expect(page).to have_content('This is a post 5')
+    expect(page).to have_content('This is a post 4')
+  end
+
+  scenario "When I click on a post, I am redirected to that post's show page" do
+    visit user_path(User.first.id) 
+    expect(page).to have_content('This is a post 6')
+
+  end
+
 end
