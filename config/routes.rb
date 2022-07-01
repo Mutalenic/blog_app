876cbd742773c673e7aeca38c0c26754adb83206
Nicholas Mutale <nicomutale@gmail.com>
2022-07-01 13:43:15 +0200

    Add routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,14 @@ Rails.application.routes.draw do
       resources :likes, only: [:create, :new]
     end
   end
+
+  namespace :api do
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :create, :new, :show, :destroy] do
+        resources :comments, only: [:create, :new, :destroy]
+        resources :likes, only: [:create, :new]
+      end
+    end
+  end
   root 'users#index'
 end
