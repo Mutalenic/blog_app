f663d0f2ccb5779d033440333d86a1aae2a3aaad
Nicholas Mutale <nicomutale@gmail.com>
2022-07-02 18:05:25 +0200

    Fix linter errors

--- spec/requests/api/my_spec.rb
+++ spec/requests/api/my_spec.rb
@@ -1,4 +1,44 @@
-require 'swagger_helper'
+# spec/swagger_helper.rb
+RSpec.configure do |config|
+  config.swagger_root = "#{Rails.root}/swagger"
 
-RSpec.describe 'api/my', type: :request do
+  config.swagger_docs = {
+    'v1/swagger.json' => {
+      openapi: '3.0.1',
+      info: {
+        title: 'API V1',
+        version: 'v1',
+        description: 'This is the first version of my API'
+      },
+      servers: [
+        {
+          url: 'https://{defaultHost}',
+          variables: {
+            defaultHost: {
+              default: 'www.example.com'
+            }
+          }
+        }
+      ]
+    },
+
+    'v2/swagger.yaml' => {
+      openapi: '3.0.1',
+      info: {
+        title: 'API V2',
+        version: 'v2',
+        description: 'This is the second version of my API'
+      },
+      servers: [
+        {
+          url: 'https://{defaultHost}',
+          variables: {
+            defaultHost: {
+              default: 'www.example.com'
+            }
+          }
+        }
+      ]
+    }
+  }
 end
